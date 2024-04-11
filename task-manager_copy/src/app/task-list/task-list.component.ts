7f08ace3b9d92f294e90695ed687bef595dafdf1
Farah-Hamdi <farah.hamdi2000@gmail.com>
2024-04-12 00:25:54 +0200

    "fourth"

--- task-manager_copy/src/app/task-list/task-list.component.ts
+++ task-manager_copy/src/app/task-list/task-list.component.ts
@@ -7,33 +7,77 @@ import { TaskService } from '../task/task.service';
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
+
 export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
-  showTaskForm: boolean = false;
+
+  task: Task = { name: '', description: '', dueDate: new Date(), status: false };
+  isEditing: boolean = false;
+  selectedTask: Task | null = null;
+  indexSelected:number=0;
+  showForm:boolean=false;
+
 
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  loadTasks(): void {
     this.tasks = this.taskService.getTasks();
   }
 
   onDeleteTask(index: number): void {
     this.taskService.deleteTask(index);
-    this.tasks = this.taskService.getTasks();
+    this.loadTasks();
+    this.isEditing = false; // Reset editing state after deleting a task
   }
 
-  onToggleTaskStatus(index: number): void {
-    const updatedTask = { ...this.tasks[index] };
-    updatedTask.status = !updatedTask.status;
-    this.taskService.updateTask(index, updatedTask);
+  onToggleTaskStatus(task: Task): void {
+    task.status = !task.status;
+    this.taskService.updateTask(this.tasks.indexOf(task), task);
   }
 
   onAddTaskClicked(): void {
-    this.showTaskForm = true;
+    //this.selectedTask = null;
+    this.showForm=true;
+    this.isEditing = true;
   }
+
+  onEditTaskClicked(i:number): void {
+    this.showForm=true;
+    //console.log("Selected task:", this.selectedTask);
+    this.isEditing = true;
+    console.log("isEditing:", this.isEditing);
+    this.selectedTask=this.taskService.getTask(i);
+    //this.isEditing=true;
+    this.indexSelected=i;
+    //this.task={ name: this.selectedTask.name, description: this.selectedTask.description, dueDate: this.selectedTask.dueDate, status: this.selectedTask.status }; // Reset the task object after submission
+
+  }
+
+
+
+
   onSubmitTaskForm(task: Task): void {
-    this.taskService.addTask(task);
-    this.tasks = this.taskService.getTasks();
-    this.showTaskForm = false;
+    if (this.selectedTask !== null) {
+      const index = this.indexSelected;
+      //this.selectedTask=null;
+      this.taskService.updateTask(index, task);
+      this.taskService.deleteTask(index);
+      this.taskService.addTask(task);
+    } else {
+      this.taskService.addTask(task);
+    }
+    this.loadTasks();
+    this.selectedTask = null;
+    this.isEditing = false;
+    this.task = { name: '', description: '', dueDate: new Date(), status: false }; // Reset the task object after submission
+    this.showForm = false;
+
+
+
   }
+
 }
